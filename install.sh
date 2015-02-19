8348cf8a7b708d0d70fea34e75690bcc67746370
Vital Kudzelka <vital.kudzelka@gmail.com>
2015-02-19 14:27:34 +0300

    Allow curl to create the necessary directory hierarchy as needed

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ fi
 
 git clone https://github.com/vitalk/sanevi ~/.vim
 
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.vim/vimrc ~/.vimrc
